8ea6187c8579a2d15029681a0b422830b36a514e
nomankhan232 <nomankhan192069@gmail.com>
2025-08-28 18:15:10 +0600

    Call history section done

--- script.js
+++ script.js
@@ -12,14 +12,40 @@ cardHearts.forEach(function (heart) {
 });
 
 
-// call btn alert
+// call btn alert and coin count
 const callButtons = document.querySelectorAll(".call-btn");
+let coinCount = document.getElementById("coin-count");
+let coins = parseInt(coinCount.textContent);
+const historyList = document.getElementById("history-list");
+const clearBtn = document.getElementById("clear-history");
+const template = document.getElementById("history-template");
 
 callButtons.forEach(function (btn) {
     btn.addEventListener("click", function () {
-        const card = btn.closest(".card");
-        const serviceName = card.querySelector("p").textContent;
-        const serviceNumber = card.querySelector(".service-number").textContent;
-        alert(`📞Calling National Emergency Service 999...`);
+        if (coins >= 20) {
+            const card = btn.closest(".card");
+            const serviceName = card.querySelector("h2").textContent;
+            const serviceNumber = card.querySelector(".service-number").textContent;
+
+            coins -= 20;
+            coinCount.textContent = coins;
+
+            const clone = template.content.cloneNode(true);
+            clone.querySelector(".service-name").textContent = serviceName;
+            clone.querySelector(".service-number").textContent = serviceNumber;
+            clone.querySelector(".service-time").textContent = new Date().toLocaleTimeString();
+
+            historyList.prepend(clone);
+
+            alert(`📞 Calling ${serviceName} (${serviceNumber})...`);
+        }
+
+        else {
+            alert("You don't have enough coins. You need at least 20 coins to make a call.")
+        }
     });
+});
+
+clearBtn.addEventListener("click", function () {
+    historyList.innerHTML = "";
 });
